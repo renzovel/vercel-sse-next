13953398002d99b09f6eee3a0542e684e8cd84cf
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-12-06 16:40:47 -0300

    teste

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
+const app = require('express')();
+
 app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item:---------------- <a href="${path}">${path}</a>`);
+    res.end(`Hello! Go to item:----------------`);
   });
   
   app.get('/api/item/:slug', (req, res) => {
