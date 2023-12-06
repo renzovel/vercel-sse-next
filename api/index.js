89347cac6d13d477e0053c7c2db95c574f34f361
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-12-06 16:45:44 -0300

    teste

--- api/index.js
+++ api/index.js
@@ -4,12 +4,11 @@ app.get('/api', (req, res) => {
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Connection","keep-alive");
-    res.end(`Hello! Go to item:----------------`);
 
-
-    
-
-
+    setInterval(() => {
+        const data = { mensaje: 'Hola desde el servidor' };
+        res.write(`data: ${JSON.stringify(data)}\n\n`);
+    }, 2000);
 });
   
 module.exports = app;
